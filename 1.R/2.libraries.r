392da15b2333032086fa84162d1f86a95681f75e
Константин <k.korablev@dtwin.ru>
2025-11-18 15:20:23 +0500

    ostov

--- 1.R/2.libraries.r
+++ 1.R/2.libraries.r
@@ -19,51 +19,6 @@ libs <- c("tarchetypes", "caret", 'Dtwin', 'expm', 'rsdmx',
           "geojsonsf","gepaf","fuzzyjoin","fresh","mapdeck","shinycssloaders",
           "rhandsontable","networkD3","httr2","shinyBS","bsplus","psycModel","pagedown","mailR", 'stripe.pay')
 
-
-# Проверка наличия и установкана библиотек ЦД и devtools
-
-tryCatch({
-  if("devtools" %!in% installed.packages()[,"Package"]) install.packages("devtools")
-  # if("Dtwin"    %!in% installed.packages()[,"Package"]) devtools::install_github("St-Digital-Twin/133.DtwinLib")
-  # if("DTwinDW"  %!in% installed.packages()[,"Package"]) devtools::install_github("St-Digital-Twin/DTwinDW")
-  # if("stripe.pay"  %!in% installed.packages()[,"Package"]) devtools::install_github("St-Digital-Twin/218.Payment")
-
-  
-  # Обновление библиотек из гитхаба при наличии
-  update_github_pkgs <- function() {
-    
-    # check/load necessary packages
-    # devtools package
-    if (!("package:devtools" %in% search())) {
-      tryCatch(require(devtools), error = function(x) {warning(x); cat("Cannot load devtools package \n")})
-      on.exit(detach("package:devtools", unload = TRUE))
-    }
-    
-    pkgs <- installed.packages(fields = "RemoteType")
-    github_pkgs <- pkgs[pkgs[, "RemoteType"] %in% "github", "Package"]
-    
-    print(github_pkgs)
-    lapply(github_pkgs, function(pac) {
-      message("Updating ", pac, " from GitHub...")
-      
-      repo = packageDescription(pac, fields = "GithubRepo")
-      username = packageDescription(pac, fields = "GithubUsername")
-      
-      install_github(repo = paste0(username, "/", repo), auth_token = "")
-    })
-  }
-  
-  # update_github_pkgs()
-  
-}, error=function(cond) {
-  message('Не удалось установить библиотеку Dtwin.\nПопробуйте установить её вручную по инструкции по ссылке: https://docs.google.com/document/d/1VHMOmU0sHxwrQ0wb74inNxWKcPPc3W6SNXlaTYdwY3A/edit?usp=sharing')
-})
-
-
-# Инсталляция отсутствующих не гитхаб библиотек
-new.packages <- libs[!(libs %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 # Включение библиотек
 suppressMessages(lapply(libs, require, character.only = TRUE))
 
